af7f1cdb9fe3a2798f40eed98baa6c01a3d8feb2
Phil Magnuson <phillmag@gmail.com>
2021-05-12 17:21:36 -0600

    added try block for new implementation

--- lib/new_version.dart
+++ lib/new_version.dart
@@ -27,19 +27,25 @@ class VersionStatus {
   // bool get canUpdate => localVersion.compareTo(storeVersion).isNegative;
   // version strings can be of the form xx.yy.zz (build)
   bool get canUpdate {
-    final localFields = localVersion.split('.');
-    final storeFields = storeVersion.split('.');
-    String localPad = '';
-    String storePad = '';
-    for (int i = 0; i < storeFields.length; i++) {
-      localPad = localPad + localFields[i].padLeft(3, '0');
-      storePad = storePad + storeFields[i].padLeft(3, '0');
+    // assume version strings can be of the form xx.yy.zz
+    // this implementation correctly compares local 1.10.1 to store 1.9.4
+    try {
+      final localFields = localVersion.split('.');
+      final storeFields = storeVersion.split('.');
+      String localPad = '';
+      String storePad = '';
+      for (int i = 0; i < storeFields.length; i++) {
+        localPad = localPad + localFields[i].padLeft(3, '0');
+        storePad = storePad + storeFields[i].padLeft(3, '0');
+      }
+      // print('new_version canUpdate local $localPad store $storePad');
+      if (localPad.compareTo(storePad) < 0)
+        return true;
+      else
+        return false;
+    } catch (e) {
+      return localVersion.compareTo(storeVersion).isNegative;
     }
-    // print('new_version canUpdate local $localPad store $storePad');
-    if (localPad.compareTo(storePad) < 0)
-      return true;
-    else
-      return false;
   }
 
   VersionStatus._({
