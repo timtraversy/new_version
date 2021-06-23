d58cd2290e2a05c964899d04994fb11c21cc7654
timtraversy <timtrav@gmail.com>
2021-06-23 10:04:13 -0400

    Add better example code to show both behavior options

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,12 +24,40 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
 
-    // Implementation is here
+    // Instantiate NewVersion manager object (Using GCP Console app as example)
     final newVersion = NewVersion(
       iOSId: 'com.google.Vespa',
       androidId: 'com.google.android.apps.cloudconsole',
     );
+
+    // You can let the plugin handle fetching the status and showing a dialog,
+    // or you can fetch the status and display your own dialog, or no dialog.
+    const simpleBehavior = true;
+
+    if (simpleBehavior) {
+      basicStatusCheck(newVersion);
+    } else {
+      advancedStatusCheck(newVersion);
+    }
+  }
+
+  basicStatusCheck(NewVersion newVersion) {
     newVersion.showAlertIfNecessary(context: context);
+  }
+
+  advancedStatusCheck(NewVersion newVersion) async {
+    final status = await newVersion.getVersionStatus();
+    debugPrint(status.releaseNotes);
+    debugPrint(status.appStoreLink);
+    debugPrint(status.localVersion);
+    debugPrint(status.storeVersion);
+    debugPrint(status.canUpdate.toString());
+    newVersion.showUpdateDialog(
+      context: context,
+      versionStatus: status,
+      dialogTitle: 'Custom Title',
+      dialogText: 'Custom Text',
+    );
   }
 
   @override
